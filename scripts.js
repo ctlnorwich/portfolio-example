c0c1d3bb8eafd8afb915a8436d830ef769a232ae
William Corner <william.corner@gmail.com>
2026-02-25 10:55:17 +0000

    Add script for scroll animations

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const inView = () => {
+  const sections = document.querySelectorAll(".scrollCheck");
+
+  if (!sections) {
+    return;
+  }
+
+  const options = {
+    threshold: 0.75,
+  };
+
+  const sectionObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("inView");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
+
+  sections.forEach(section => {
+    sectionObserver.observe(section);
+  });
+};
+
+inView();
